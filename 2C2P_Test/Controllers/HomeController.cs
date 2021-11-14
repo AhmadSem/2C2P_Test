6e3145238c05985bff47b35fa0a86556e5b77a4a
AhmadSemesmieh <Ahmad.Semesmieh@outlook.com>
2021-11-14 19:46:33 +0800

    Clearing some files.

--- 2C2P_Test/Controllers/HomeController.cs
+++ 2C2P_Test/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace _2C2P_Test.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
